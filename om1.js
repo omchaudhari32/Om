7ae2a1703c7c16a4679a89d55f6210d3eb59c387
omchaudhari32 <omchaudhari32@github.com>
2023-03-17 12:51:03 +0530

    update

---
+++ om1.js
@@ -0,0 +1,34 @@
+//Q1  
+var array =[34,45,65,23,5,676,32,34]
+ var target = 66
+ for(i=0; i<=array length; i++ ){
+ for(j=0; j<=array length; j++ ){
+if (array[i] + array[j] = = = target)
+flag = true;
+if(flag = true){
+   else (not Found Number)
+} 
+}
+}
+
+
+
+
+//Q2 
+var start = 5768;
+var end = 78656;
+var nums= 0;
+for(var i= 0; i<= array length; i=+2){
+    console.log(addition of number )
+
+}
+
+//Q4 
+var array =[34,45,567,78,89,34,67,87,34,78,89]
+var object=[]
+for(var i= 0; i<length;i++){
+    if(array )
+}
+if(0:34, 1:45, 2: 567, 3: 78, 4: 89, 5: 34, 6: 67, 7:87,8:34,9:78,10:89){
+    array 
+}
